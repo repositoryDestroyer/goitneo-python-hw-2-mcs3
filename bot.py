7748f87eab14589d078bd0e8b9d79f35ea7ec3a3
Mykyta Ostapenko <nikitaostapenko12@gmail.com>
2023-10-22 21:12:11 +0300

    upgrade bot

--- bot.py
+++ bot.py
@@ -1,37 +1,68 @@
+class EmptyListError(Exception):
+    pass
+
+
+class IvalidArgsNumberError(Exception):
+    pass
+
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
 
+def input_error_handler(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Such a user does not exist."
+        except EmptyListError:
+            return "Contacts list is empty."
+        except IvalidArgsNumberError:
+            return "Invalid number of arguments."
+    return inner
+
+
+@input_error_handler
 def add_contact(args, contacts):
+    if len(args) != 2:
+        raise IvalidArgsNumberError
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
 
+@input_error_handler
 def change_contact(args, contacts):
+    if len(args) != 2:
+        raise IvalidArgsNumberError
     name, phone = args
-    if name not in contacts:
-        return "Such a user does not exist."
-    else:
-        contacts[name] = phone
-        return "Contact updated."
+    contacts[name] = phone
+    return "Contact updated."
 
 
+@input_error_handler
 def show_phone(args, contacts):
+    if len(args) != 1:
+        raise IvalidArgsNumberError
     name = args[0]
     if name not in contacts:
-        return "Such a user does not exist"
+        raise KeyError
     else:
         return contacts[name]
 
 
-def show_all(contacts):
-    # contacts_list = [f"{name}: {number}" for name, number in contacts.items()]
-    # contacts_list = ("\n".join(contacts_list))
+@input_error_handler
+def show_all(args, contacts):
+    if len(args) != 0:
+        raise IvalidArgsNumberError
+
     if len(contacts.keys()) < 1:
-        return "Contacts list is empty."
+        raise EmptyListError
 
     contacts_list = []
 
@@ -61,7 +92,7 @@ def main():
         elif command == "phone":
             print(show_phone(args, contacts))
         elif command == "all":
-            print(show_all(contacts))
+            print(show_all(args, contacts))
         else:
             print("Invalid command.")
 
